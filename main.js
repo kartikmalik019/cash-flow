c65f0bdfdca608e46b9c0fa292a172faa116e253
Prernabudh <prernabudhraja8@gmail.com>
2020-07-19 00:32:31 +0530

    Random nodes added

--- main.js
+++ main.js
@@ -1,110 +1,126 @@
 let map = new Map();
 let nodes = [];
 let edges = [];
-const from = []
-const to = []
-for(let i = 0;i<from.length;i++){
-    if(!map.has(from[i])){
-        nodes.push({id:from[i],label:"Node "+from[i]})
-    }
-    map.set(from[i],new Set())
+const from = [];
+const to = [];
+for (let i = 0; i < from.length; i++) {
+  if (!map.has(from[i])) {
+    nodes.push({ id: from[i], label: "Node " + from[i] });
+  }
+  map.set(from[i], new Set());
 }
-for(let i = 0;i<from.length;i++){
-    map.get(from[i]).add(to[i])
-    edges.push({from:from[i],to:to[i],label:"55"})
+for (let i = 0; i < from.length; i++) {
+  map.get(from[i]).add(to[i]);
+  edges.push({ from: from[i], to: to[i], label: "55" });
 }
-
 
 // load graphs
-const loadGraph = (N,E)=>{
-    var nodes = new vis.DataSet(N);
-    var edges = new vis.DataSet(E);
-    var container = document.getElementById('mynetwork');
-    var data = {
-        nodes: nodes,
-        edges: edges
-    };
-    var options = {
-        edges:{
-            arrows:{to:true}
-        }
-    };
-    var network = new vis.Network(container, data, options);
-}
+const loadGraph = (N, E) => {
+  var nodes = new vis.DataSet(N);
+  var edges = new vis.DataSet(E);
+  var container = document.getElementById("mynetwork");
+  var data = {
+    nodes: nodes,
+    edges: edges,
+  };
+  var options = {
+    edges: {
+      arrows: { to: true },
+    },
+  };
+  var network = new vis.Network(container, data, options);
+};
 
-const loadSimplifiedGraph = (N,E)=>{
-    var nodes = new vis.DataSet(N);
-    var edges = new vis.DataSet(E);
-    var container = document.getElementById('mynetwork2');
-    var data = {
-        nodes: nodes,
-        edges: edges
-    };
-    var options = {
-        edges:{
-            arrows:{to:true}
-        }
-    };
-    var network = new vis.Network(container, data, options);
-}
-
+const loadSimplifiedGraph = (N, E) => {
+  var nodes = new vis.DataSet(N);
+  var edges = new vis.DataSet(E);
+  var container = document.getElementById("mynetwork2");
+  var data = {
+    nodes: nodes,
+    edges: edges,
+  };
+  var options = {
+    edges: {
+      arrows: { to: true },
+    },
+  };
+  var network = new vis.Network(container, data, options);
+};
 
 // utils
-const addNode=(node,map,nodes)=>{
-    if(!map.has(node)){
-        nodes.push({id:node,label:"Node "+node})
-        map.set(node,new Set())
-    }
-    loadGraph(nodes,edges)
-}
-const addEdge = (from,to,value,map,edges) =>{
-    map.get(from).add(to)
-    edges.push({from:from,to:to,label:value.toString()})
-    loadGraph(nodes,edges)
-}
-loadGraph(nodes,edges)
+const addNode = (node, map, nodes) => {
+  if (!map.has(node)) {
+    nodes.push({ id: node, label: "Node " + node });
+    map.set(node, new Set());
+  }
+  loadGraph(nodes, edges);
+};
+const addEdge = (from, to, value, map, edges) => {
+  map.get(from).add(to);
+  edges.push({ from: from, to: to, label: value.toString() });
+  loadGraph(nodes, edges);
+};
+loadGraph(nodes, edges);
 
-const addNodeClick = () =>{
-    const node = parseInt(document.getElementById("new-node").value,10)
-    document.getElementById("new-node").value = ""
-    addNode(node,map,nodes)
-}
+const addNodeClick = () => {
+  const node = parseInt(document.getElementById("new-node").value, 10);
+  document.getElementById("new-node").value = "";
+  addNode(node, map, nodes);
+};
 
-const addEdgeClick = () =>{
-    const from = parseInt(document.getElementById("from").value,10)
-    document.getElementById("from").value = ""
-    const to = parseInt(document.getElementById("to").value,10)
-    document.getElementById("to").value = ""
-    const value = parseInt(document.getElementById("value").value,10)
-    document.getElementById("value").value = ""
-    addEdge(from,to,value,map,edges)
-}
+const addEdgeClick = () => {
+  const from = parseInt(document.getElementById("from").value, 10);
+  document.getElementById("from").value = "";
+  const to = parseInt(document.getElementById("to").value, 10);
+  document.getElementById("to").value = "";
+  const value = parseInt(document.getElementById("value").value, 10);
+  document.getElementById("value").value = "";
+  addEdge(from, to, value, map, edges);
+};
 
+const createRandom = () => {
+  const n = parseInt(document.getElementById("nodes").value, 10);
+  document.getElementById("nodes").value = "";
+  for (let i = 0; i < n; i++) addNode(i, map, nodes);
+  let j = 0;
+  while (j < 2 * n) {
+    let a = Math.floor(Math.random() * (n - 1 - 0 + 1)) + 0;
+    let b = Math.floor(Math.random() * (n - 1 - 0 + 1)) + 0;
+    let val = Math.floor(Math.random() * 10 + 10) * 100;
+    if (a == b) continue;
+    addEdge(a, b, val, map, edges);
+    j++;
+  }
+};
 
 // cash flow minimization
-const simplify = () =>{
-    const minCashFlow = () =>{
-        let amount = []
-        for(let i = 0;i<=nodes.length;i++){
-            amount.push(0)
-        }
-        for(let i = 0;i<edges.length;i++){
-            amount[edges[i].from] -= parseInt(edges[i].label,10)
-            amount[edges[i].to] += parseInt(edges[i].label,10)
-        }
-        amount.shift()
-        let mxCredit = amount.indexOf(Math.max(...amount))
-        let mxDebit = amount.indexOf(Math.min(...amount))
-        let newEdges = []
-        while(!amount[mxCredit] == 0 && !amount[mxDebit] == 0){
-            const min = Math.min(-amount[mxDebit], amount[mxCredit]); 
-            amount[mxCredit] -= min; 
-            amount[mxDebit] += min;
-            newEdges.push({from:mxDebit+1,to:mxCredit+1,label:min.toString()})
-            mxCredit = amount.indexOf(Math.max(...amount))
-            mxDebit = amount.indexOf(Math.min(...amount))
-        }
-        loadSimplifiedGraph(nodes,newEdges)
+const simplify = () => {
+  const minCashFlow = () => {
+    let amount = [];
+    for (let i = 0; i <= nodes.length; i++) {
+      amount.push(0);
     }
-    minCashFlow()
-}
+    for (let i = 0; i < edges.length; i++) {
+      amount[edges[i].from] -= parseInt(edges[i].label, 10);
+      amount[edges[i].to] += parseInt(edges[i].label, 10);
+    }
+    amount.shift();
+    let mxCredit = amount.indexOf(Math.max(...amount));
+    let mxDebit = amount.indexOf(Math.min(...amount));
+    let newEdges = [];
+    while (!amount[mxCredit] == 0 && !amount[mxDebit] == 0) {
+      const min = Math.min(-amount[mxDebit], amount[mxCredit]);
+      amount[mxCredit] -= min;
+      amount[mxDebit] += min;
+      newEdges.push({
+        from: mxDebit + 1,
+        to: mxCredit + 1,
+        label: min.toString(),
+      });
+      mxCredit = amount.indexOf(Math.max(...amount));
+      mxDebit = amount.indexOf(Math.min(...amount));
+    }
+    loadSimplifiedGraph(nodes, newEdges);
+  };
+  minCashFlow();
+};
